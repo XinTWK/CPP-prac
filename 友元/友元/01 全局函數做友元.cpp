4786e4ea397087c258b3549b5b94af490537206b
XinTWK <fly4742@gmail.com>
2021-07-06 01:28:09 +0800

    rename

--- 友元/友元/01 全局函數做友元.cpp
+++ 友元/友元/01 全局函數做友元.cpp
@@ -1,50 +1,50 @@
-//#define _CRT_SECURE_NO_WARNINGS
-//#include<iostream>
-//using namespace std;
-//#include<string>
-//
-//// 建築物類
-//class Building
-//{
-//	// goodGay全局函數是 Building好朋友, 可以訪問Building中私有成員
-//	friend void goodGay(Building *building);
-//public:
-//	Building()
-//	{
-//		m_SittingRomm = "客廳";
-//		m_BedRoom = "臥室";
-//	}
-//
-//public:
-//	string m_SittingRomm; // 客廳
-//
-//private:
-//	string m_BedRoom; // 臥室
-//};
-//
-//
-//// 全局函數
-//void goodGay(Building *building) 
-//{
-//	cout << "好基友全局函數  正在訪問 : " << building->m_SittingRomm << endl;
-//
-//	cout << "好基友全局函數  正在訪問 : " << building->m_BedRoom << endl;
-//
-//}
-//
-//void test01()
-//{
-//	Building building;
-//	goodGay(&building);
-//}
-//
-//
-//
-//int main() {
-//
-//	test01();
-//
-//	system("pause");
-//
-//	return EXIT_SUCCESS;
-//}
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+using namespace std;
+#include<string>
+
+// 建築物類
+class Building
+{
+	// goodGay全局函數是 Building好朋友, 可以訪問Building中私有成員
+	friend void goodGay(Building *building);
+public:
+	Building()
+	{
+		m_SittingRomm = "客廳";
+		m_BedRoom = "臥室";
+	}
+
+public:
+	string m_SittingRomm; // 客廳
+
+private:
+	string m_BedRoom; // 臥室
+};
+
+
+// 全局函數
+void goodGay(Building *building) 
+{
+	cout << "好基友全局函數  正在訪問 : " << building->m_SittingRomm << endl;
+
+	cout << "好基友全局函數  正在訪問 : " << building->m_BedRoom << endl;
+
+}
+
+void test01()
+{
+	Building building;
+	goodGay(&building);
+}
+
+
+
+int main() {
+
+	test01();
+
+	system("pause");
+
+	return EXIT_SUCCESS;
+}
--- 引用/引用/01 引用的基本語法.cpp
+++ 引用/引用/01 引用的基本語法.cpp
@@ -1,26 +1,26 @@
-//#include <iostream>
-//using namespace std;
-//
-//
-//
-//int main()
-//{
-//	//引用基本語法
-//	//數據類型 &別名 = 原名
-//
-//	int a = 10;
-//	//創建引用
-//	int &b = a;
-//
-//	cout << "a = " << a << endl;
-//	cout << "b = " << b << endl;
-//
-//	b = 100;
-//
-//	cout << "a = " << a << endl;
-//	cout << "b = " << b << endl;
-//
-//	system("pause");
-//
-//	return 0;
-//}
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+	//引用基本語法
+	//數據類型 &別名 = 原名
+
+	int a = 10;
+	//創建引用
+	int &b = a;
+
+	cout << "a = " << a << endl;
+	cout << "b = " << b << endl;
+
+	b = 100;
+
+	cout << "a = " << a << endl;
+	cout << "b = " << b << endl;
+
+	system("pause");
+
+	return 0;
+}
