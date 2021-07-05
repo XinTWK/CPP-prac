076cf9a558ca5873b54a29654c662ba479448380
XinTWK <fly4742@gmail.com>
2021-07-06 01:29:25 +0800

    註釋取消

--- 文本文件/文本文件/02 文本文件-讀文件.cpp
+++ 文本文件/文本文件/02 文本文件-讀文件.cpp
@@ -1,67 +1,67 @@
-//#define _CRT_SECURE_NO_WARNINGS
-//#include<iostream>
-//using namespace std;
-//#include<fstream>
-//#include<string>
-//
-//// 文本文件 讀文件
-//void test01()
-//{
-//	// 1. 包含頭文件
-//
-//	// 2. 創建流對象
-//	ifstream ifs;
-//
-//	// 3. 打開文件 並且判斷是否打開成功
-//	ifs.open("test.txt", ios::in);
-//
-//	if (!ifs.is_open())
-//	{
-//		cout << "文件打開失敗" << endl;
-//		return;
-//	}
-//
-//	// 4. 讀數據
-//
-//	// 第一種
-//	//char buf[1024] = { 0 };
-//	//while (ifs >> buf)
-//	//{
-//	//	cout << buf << endl;
-//	//}
-//
-//	// 第二種
-//	//char buf[1024] = { 0 };
-//	//while (ifs.getline(buf, sizeof(buf)))
-//	//{
-//	//	cout << buf << endl;
-//	//}
-//
-//	// 第三種
-//	//string buf;
-//	//while (getline(ifs, buf))
-//	//{
-//	//	cout << buf << endl;
-//	//}
-//
-//	// 第四種
-//	char c;
-//	while ((c = ifs.get()) != EOF) // EOF : End Of File
-//	{
-//		cout << c;
-//	}
-//
-//
-//	// 5. 關閉文件
-//	ifs.close();
-//}
-//
-//
-//int main() {
-//
-//	test01();
-//
-//	system("pause");
-//
-//	return EXIT_SUCCESS;
-//}
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+using namespace std;
+#include<fstream>
+#include<string>
+
+// 文本文件 讀文件
+void test01()
+{
+	// 1. 包含頭文件
+
+	// 2. 創建流對象
+	ifstream ifs;
+
+	// 3. 打開文件 並且判斷是否打開成功
+	ifs.open("test.txt", ios::in);
+
+	if (!ifs.is_open())
+	{
+		cout << "文件打開失敗" << endl;
+		return;
+	}
+
+	// 4. 讀數據
+
+	// 第一種
+	//char buf[1024] = { 0 };
+	//while (ifs >> buf)
+	//{
+	//	cout << buf << endl;
+	//}
+
+	// 第二種
+	//char buf[1024] = { 0 };
+	//while (ifs.getline(buf, sizeof(buf)))
+	//{
+	//	cout << buf << endl;
+	//}
+
+	// 第三種
+	//string buf;
+	//while (getline(ifs, buf))
+	//{
+	//	cout << buf << endl;
+	//}
+
+	// 第四種
+	char c;
+	while ((c = ifs.get()) != EOF) // EOF : End Of File
+	{
+		cout << c;
+	}
+
+
+	// 5. 關閉文件
+	ifs.close();
+}
+
+
+int main() {
+
+	test01();
+
+	system("pause");
+
+	return EXIT_SUCCESS;
+}
--- 文本文件/文本文件/03 二進制文件-寫文件.cpp
+++ 文本文件/文本文件/03 二進制文件-寫文件.cpp
@@ -1,43 +1,43 @@
-//#define _CRT_SECURE_NO_WARNINGS
-//#include<iostream>
-//using namespace std;
-//#include<fstream>
-//
-//// 二進制文件  寫文件
-//class Person
-//{
-//public:
-//
-//	char m_Name[64]; // 姓名
-//	int m_Age; // 年齡
-//};
-//
-//
-//void test01()
-//{
-//	// 1. 包含頭文件
-//
-//	// 2. 創建流對象
-//	ofstream ofs("person.txt", ios::out | ios::binary);
-//
-//	// 3. 打開文件
-//	//ofs.open("person.txt", ios::out | ios::binary);
-//
-//	// 4. 寫文件
-//	Person p = { "張三",18 };
-//	ofs.write((const char *)&p, sizeof(Person));
-//
-//	// 5. 關閉文件
-//	ofs.close();
-//
-//}
-//
-//
-//int main() {
-//
-//	test01();
-//
-//	system("pause");
-//
-//	return EXIT_SUCCESS;
-//}
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+using namespace std;
+#include<fstream>
+
+// 二進制文件  寫文件
+class Person
+{
+public:
+
+	char m_Name[64]; // 姓名
+	int m_Age; // 年齡
+};
+
+
+void test01()
+{
+	// 1. 包含頭文件
+
+	// 2. 創建流對象
+	ofstream ofs("person.txt", ios::out | ios::binary);
+
+	// 3. 打開文件
+	//ofs.open("person.txt", ios::out | ios::binary);
+
+	// 4. 寫文件
+	Person p = { "張三",18 };
+	ofs.write((const char *)&p, sizeof(Person));
+
+	// 5. 關閉文件
+	ofs.close();
+
+}
+
+
+int main() {
+
+	test01();
+
+	system("pause");
+
+	return EXIT_SUCCESS;
+}
