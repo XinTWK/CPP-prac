2846d95bc547b45dbea421387e1751057fa210e8
XinTWK <fly4742@gmail.com>
2021-07-06 01:37:44 +0800

    註釋取消

--- 對象的初始化和清理/對象的初始化和清理/07 類對象作為類成員.cpp
+++ 對象的初始化和清理/對象的初始化和清理/07 類對象作為類成員.cpp
@@ -1,65 +1,65 @@
-//#include<iostream>
-//using namespace std;
-//#include<string>
-//
-//// 類對象作為類成員
-//
-//// 手機類
-//class Phone
-//{
-//public:
-//
-//	Phone(string pName)
-//	{
-//		cout << "Phone的構造函數調用" << endl;
-//
-//		m_PName = pName;
-//	}
-//
-//	~Phone()
-//	{
-//		cout << "Phone的析構函數調用" << endl;
-//
-//	}
-//
-//	// 手機品牌名稱
-//	string m_PName;
-//};
-//
-//// 人類
-//class Person
-//{
-//public:
-//	// Phone m_Phone = pName  隱式轉換法
-//	Person(string name, string pName) : m_Name(name),m_phone(pName) 
-//	{
-//		cout << "Person的構造函數調用" << endl;
-//	}
-//
-//	~Person()
-//	{
-//		cout << "Person的析構函數調用" << endl;
-//	}
-//	// 姓名
-//	string m_Name;
-//	// 手機
-//	Phone m_phone;
-//};
-//
-//// 當其他類對象作為本類成員, 構造時候先構造類對象, 再構造自身, 析構的順序與構造相反
-//
-//void test01()
-//{
-//	Person p("張三", "蘋果MAX");
-//
-//	cout << p.m_Name << "拿著:  " << p.m_phone.m_PName << endl;
-//}
-//
-//int main() {
-//
-//	test01();
-//
-//	system("pause");
-//
-//	return 0;
-//}
+#include<iostream>
+using namespace std;
+#include<string>
+
+// 類對象作為類成員
+
+// 手機類
+class Phone
+{
+public:
+
+	Phone(string pName)
+	{
+		cout << "Phone的構造函數調用" << endl;
+
+		m_PName = pName;
+	}
+
+	~Phone()
+	{
+		cout << "Phone的析構函數調用" << endl;
+
+	}
+
+	// 手機品牌名稱
+	string m_PName;
+};
+
+// 人類
+class Person
+{
+public:
+	// Phone m_Phone = pName  隱式轉換法
+	Person(string name, string pName) : m_Name(name),m_phone(pName) 
+	{
+		cout << "Person的構造函數調用" << endl;
+	}
+
+	~Person()
+	{
+		cout << "Person的析構函數調用" << endl;
+	}
+	// 姓名
+	string m_Name;
+	// 手機
+	Phone m_phone;
+};
+
+// 當其他類對象作為本類成員, 構造時候先構造類對象, 再構造自身, 析構的順序與構造相反
+
+void test01()
+{
+	Person p("張三", "蘋果MAX");
+
+	cout << p.m_Name << "拿著:  " << p.m_phone.m_PName << endl;
+}
+
+int main() {
+
+	test01();
+
+	system("pause");
+
+	return 0;
+}
--- 對象的初始化和清理/對象的初始化和清理/09 靜態成員-靜態成員函數.cpp
+++ 對象的初始化和清理/對象的初始化和清理/09 靜態成員-靜態成員函數.cpp
@@ -1,54 +1,54 @@
-//#include<iostream>
-//using namespace std;
-//
-//// 靜態成員函數
-//// 所有對象都共享同一個函數
-//// 靜態成員函數只能訪問靜態成員變量
-//
-//class Person
-//{
-//public:
-//	// 靜態成員函數
-//	static void func()
-//	{
-//		m_A = 100;  // 靜態成員函數可以訪問 靜態成員變量
-//		//m_B = 200;  // 靜態成員函數 不可以訪問 非靜態成員變量, 無法區分到底是哪個對象的m_B的屬性
-//		cout << "static void func調用" << endl;
-//	}
-//
-//	static int m_A; // 靜態成員變量
-//	int m_B; // 非靜態成員變量
-//
-//	// 靜態成員函數也是有訪問權限的
-//private:
-//	static void func2()
-//	{
-//		cout << "static void func2調用" << endl;
-//	}
-//};
-//
-//int Person::m_A = 0;
-//
-//void test01()
-//{
-//	// 1.通過對象訪問
-//	Person p;
-//	p.func();
-//
-//	// 2.通過類名訪問
-//	Person::func();
-//
-//	//Person::func2();  類外訪問步道私有靜態成員函數
-//}
-//
-//
-//
-//int main() {
-//
-//	test01();
-//
-//
-//	system("pause");
-//
-//	return EXIT_SUCCESS;
-//}
+#include<iostream>
+using namespace std;
+
+// 靜態成員函數
+// 所有對象都共享同一個函數
+// 靜態成員函數只能訪問靜態成員變量
+
+class Person
+{
+public:
+	// 靜態成員函數
+	static void func()
+	{
+		m_A = 100;  // 靜態成員函數可以訪問 靜態成員變量
+		//m_B = 200;  // 靜態成員函數 不可以訪問 非靜態成員變量, 無法區分到底是哪個對象的m_B的屬性
+		cout << "static void func調用" << endl;
+	}
+
+	static int m_A; // 靜態成員變量
+	int m_B; // 非靜態成員變量
+
+	// 靜態成員函數也是有訪問權限的
+private:
+	static void func2()
+	{
+		cout << "static void func2調用" << endl;
+	}
+};
+
+int Person::m_A = 0;
+
+void test01()
+{
+	// 1.通過對象訪問
+	Person p;
+	p.func();
+
+	// 2.通過類名訪問
+	Person::func();
+
+	//Person::func2();  類外訪問步道私有靜態成員函數
+}
+
+
+
+int main() {
+
+	test01();
+
+
+	system("pause");
+
+	return EXIT_SUCCESS;
+}
