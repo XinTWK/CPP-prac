55a75abdb571dd2cad2437f14533da2477fc78ce
XinTWK <fly4742@gmail.com>
2021-07-06 01:21:46 +0800

    註釋取消

--- C++對象模型和this指針/C++對象模型和this指針/03 空指針調用成員函數.cpp
+++ C++對象模型和this指針/C++對象模型和this指針/03 空指針調用成員函數.cpp
@@ -1,49 +1,49 @@
-//#define _CRT_SECURE_NO_WARNINGS
-//#include<iostream>
-//using namespace std;
-//
-//// 空指針調用成員函數
-//
-//class Person
-//{
-//public:
-//
-//	void showClassName()
-//	{
-//		cout << "this is Person Class" << endl;
-//	}
-//
-//	void showPersonAge()
-//	{
-//		// 報錯原因是因為傳入的指針是為NULL
-//		if (this == NULL)
-//		{
-//			return;
-//		}
-//		cout << "age =  " << this->m_Age << endl;
-//	}
-//
-//	int m_Age;
-//};
-//
-//
-//void test01()
-//{
-//	Person * p = NULL;
-//
-//	p->showClassName();
-//
-//	p->showPersonAge();
-//
-//}
-//
-//
-//
-//int main() {
-//
-//	test01();
-//
-//	system("pause");
-//
-//	return EXIT_SUCCESS;
-//}
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+using namespace std;
+
+// 空指針調用成員函數
+
+class Person
+{
+public:
+
+	void showClassName()
+	{
+		cout << "this is Person Class" << endl;
+	}
+
+	void showPersonAge()
+	{
+		// 報錯原因是因為傳入的指針是為NULL
+		if (this == NULL)
+		{
+			return;
+		}
+		cout << "age =  " << this->m_Age << endl;
+	}
+
+	int m_Age;
+};
+
+
+void test01()
+{
+	Person * p = NULL;
+
+	p->showClassName();
+
+	p->showPersonAge();
+
+}
+
+
+
+int main() {
+
+	test01();
+
+	system("pause");
+
+	return EXIT_SUCCESS;
+}
